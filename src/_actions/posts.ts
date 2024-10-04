0a148d167a7c58049272e67bc39db1914f788494
EvansRobbie <evansrobbie5311@gmail.com>
2024-10-04 10:56:29 +0300

    relation filters

--- src/_actions/posts.ts
+++ src/_actions/posts.ts
@@ -4,25 +4,47 @@ import prisma from '@/lib/prisma';
 
 export const getPosts = async () => {
   try {
+    // const res = await prisma.post.findMany({
+    //   where:{
+    //     OR:[
+    //       {
+    //         title:{
+    //           contains:"github",
+    //           mode:"insensitive" // postgresql & mongodb
+    //         }
+    //       },{
+    //         title:{
+    //           contains:"Twitter"
+    //         }
+    //       }
+    //     ],
+    //     AND:{
+    //       published:true
+    //     }
+    //   }
+    // });
+
+    //relation filters
+
     const res = await prisma.post.findMany({
       where:{
-        OR:[
-          {
-            title:{
-              contains:"github",
-              mode:"insensitive" // postgresql & mongodb
-            }
-          },{
-            title:{
-              contains:"Twitter"
+        author:{
+          isNot:{
+            name: "Jack"
+          },
+          is:{
+            email:{
+              startsWith:"s"
             }
           }
-        ],
-        AND:{
-          published:true
-        }
+        },
+        
+
+      },
+      include:{
+        author:true
       }
-    });
+    })
     return res;
   } catch (error) {
     throw error;
--- src/_actions/users.ts
+++ src/_actions/users.ts
@@ -4,38 +4,55 @@ import prisma from '@/lib/prisma';
 
 export const getUsers = async () => {
   try {
-    const res = await prisma.user.findMany({
+    // const res = await prisma.user.findMany({
+    //   where:{
+    //     // name: {
+    //     //   startsWith:"s",
+    //     //   contains:"a"
+    //     // }
+    //   //  id:{
+    //   //     // in:[1,2]
+    //   //     // notIn:[1,2]
+    //   //     not:{
+    //   //        // in:[1,2]
+    //   //       gt:3
+    //   //     }
+    //   //   }
+    //   OR:[
+    //     {
+    //       id:{
+    //         gt:3
+    //       }
+    //     },
+    //     {
+    //       name:{
+    //         contains:"s"
+    //       }
+    //     }
+    //   ]
+    //   },
+    //   orderBy:{
+    //     name:"asc"
+    //   }
+    // });
+
+
+    // relational filters
+
+    const users = await prisma.user.findMany({
       where:{
-        // name: {
-        //   startsWith:"s",
-        //   contains:"a"
-        // }
-      //  id:{
-      //     // in:[1,2]
-      //     // notIn:[1,2]
-      //     not:{
-      //        // in:[1,2]
-      //       gt:3
-      //     }
-      //   }
-      OR:[
-        {
-          id:{
-            gt:3
-          }
-        },
-        {
-          name:{
-            contains:"s"
+        posts:{
+          //all => every
+          //any => some
+          none:{
+            published:false
           }
         }
-      ]
       },
-      orderBy:{
-        name:"asc"
-      }
+    
     });
-    return res;
+
+    return users ;
   } catch (error) {
     throw error;
   }
